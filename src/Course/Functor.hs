b54adef57ae7c95b9efd479961942cd17badbf59
milad <mza1989@gmail.com>
2016-12-14 16:29:51 +0330

    finished excercises for Course.Functor

--- src/Course/Functor.hs
+++ src/Course/Functor.hs
@@ -41,8 +41,7 @@ instance Functor Id where
     (a -> b)
     -> Id a
     -> Id b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance Id"
+  (<$>) f (Id a) = Id (f a)
 
 -- | Maps a function on the List functor.
 --
@@ -56,8 +55,7 @@ instance Functor List where
     (a -> b)
     -> List a
     -> List b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance List"
+  (<$>) = map
 
 -- | Maps a function on the Optional functor.
 --
@@ -71,8 +69,7 @@ instance Functor Optional where
     (a -> b)
     -> Optional a
     -> Optional b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance Optional"
+  (<$>) = mapOptional
 
 -- | Maps a function on the reader ((->) t) functor.
 --
@@ -83,8 +80,7 @@ instance Functor ((->) t) where
     (a -> b)
     -> (->) t a
     -> (->) t b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#((->) t)"
+  (<$>) f g = f.g
 
 -- | Anonymous map. Maps a constant value on a functor.
 --
@@ -99,8 +95,7 @@ instance Functor ((->) t) where
   a
   -> f b
   -> f a
-(<$) =
-  error "todo: Course.Functor#(<$)"
+(<$) a fb = const a <$> fb
 
 -- | Anonymous map producing unit value.
 --
@@ -119,8 +114,7 @@ void ::
   Functor f =>
   f a
   -> f ()
-void =
-  error "todo: Course.Functor#void"
+void fa = () <$ fa
 
 -----------------------
 -- SUPPORT LIBRARIES --
